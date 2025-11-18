dc3ff55e3cae96db1eafbe72a30cd71aca704527
IvanVoshchepynets <ivan.voshchepynets.ki.2019@lpnu.ua>
2025-11-18 11:07:50 +0200

    Initial commit: setup Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="bg-[#0E0E0E] text-white">
+      <Header />
+      <Hero />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
